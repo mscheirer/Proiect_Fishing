bb552d25b8198f1ea7dce5b1af66e12dfae283d5
Scheirer Nicolae (Miki) <58042678+mscheirer@users.noreply.github.com>
2020-08-28 00:05:04 +0300

    sd

--- Fishing/Utility/SD.cs
+++ Fishing/Utility/SD.cs
@@ -10,9 +10,9 @@ namespace Fishing.Utility
         public const string DefaultFishImage = "default_fisherman.jpg";
 
         public const string ManagerUser = "Manager";
-        public const string KitchenUser = "Kitchen";
-        public const string FrontDeskUser = "FrontDesk";
-        public const string CustomerEndUser = "Customer";
+        public const string KitchenUser = "Sef";
+        public const string FrontDeskUser = "Casier";
+        public const string CustomerEndUser = "Client";
 
     }
 }
