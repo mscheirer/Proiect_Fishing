81e0a2a419adc9a265efea5fb5c2ad40f931ba8a
Scheirer Nicolae (Miki) <58042678+mscheirer@users.noreply.github.com>
2020-08-29 01:17:44 +0300

    Session1

--- Fishing/Areas/Customer/Controllers/HomeController.cs
+++ Fishing/Areas/Customer/Controllers/HomeController.cs
@@ -40,6 +40,17 @@ namespace Fishing.Controllers
                 Coupon = await _db.Coupon.Where(c => c.IsActive == true).ToListAsync()
             };
 
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if(claim!=null)
+            {
+                var cnt = _db.ShoppingCart.Where(u => u.ApplicationUserId == claim.value).ToList().Count;
+                HttpContext.Session.SetInt32("ssCartCount", cnt);
+
+            }
+
+
             return View(IndexVM);
         }
 
