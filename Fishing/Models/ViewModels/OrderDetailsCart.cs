558e954dd467623a4cebd09a6bea6d8850753c95
Scheirer Nicolae (Miki) <58042678+mscheirer@users.noreply.github.com>
2020-08-29 22:58:32 +0300

    Shopping Cart - ok

---
+++ Fishing/Models/ViewModels/OrderDetailsCart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fishing.Models.ViewModels
+{
+    public class OrderDetailsCart
+    {
+        public List<ShoppingCart> listCart { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+
+
+
+
+    }
+}
--- Fishing/Utility/SD.cs
+++ Fishing/Utility/SD.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fishing.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,5 +17,81 @@ namespace Fishing.Utility
 
         public const string ssShoppingCartCount = "ssCartCount";
 
+        public const string ssCouponCode = "ssCouponCode";
+
+        public const string StatusSubmitted = "Submitted";
+        public const string StatusInProcess = "Being Prepared";
+        public const string StatusReady = "Ready for Pickup";
+        public const string StatusCompleted = "Completed";
+        public const string StatusCancelled = "Cancelled";
+
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusRejected = "Rejected";
+
+
+
+
+
+        public static string ConvertToRawHtml(string source)
+        {
+            char[] array = new char[source.Length];
+            int arrayIndex = 0;
+            bool inside = false;
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                char let = source[i];
+                if (let == '<')
+                {
+                    inside = true;
+                    continue;
+                }
+                if (let == '>')
+                {
+                    inside = false;
+                    continue;
+                }
+                if (!inside)
+                {
+                    array[arrayIndex] = let;
+                    arrayIndex++;
+                }
+            }
+            return new string(array, 0, arrayIndex);
+        }
+
+
+        public static double DiscountedPrice(Coupon couponFromDb, double OriginalOrderTotal)
+        {
+            if (couponFromDb == null)
+            {
+                return OriginalOrderTotal;
+            }
+            else
+            {
+                if (couponFromDb.MinimumAmount > OriginalOrderTotal)
+                {
+                    return OriginalOrderTotal;
+                }
+                else
+                {
+                    //everything is valid
+                    if (Convert.ToInt32(couponFromDb.CouponType) == (int)Coupon.ECouponType.Leu)
+                    {
+                        //10 lei la 100 lei
+                        return Math.Round(OriginalOrderTotal - couponFromDb.Discount, 2);
+                    }
+                    if (Convert.ToInt32(couponFromDb.CouponType) == (int)Coupon.ECouponType.Percent)
+                    {
+                        //10% din 100 lei
+                        return Math.Round(OriginalOrderTotal - (OriginalOrderTotal * couponFromDb.Discount / 100), 2);
+                    }
+                }
+            }
+            return OriginalOrderTotal;
+        }
+
     }
 }
+
