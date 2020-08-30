351e88fbf187a0ff21a36f58fd86e57c8455b9b5
Scheirer Nicolae (Miki) <58042678+mscheirer@users.noreply.github.com>
2020-08-30 19:33:10 +0300

    OrderController, OrderHistory, OrderListViewModel, PageLinkTagHelper

--- Fishing/Areas/Customer/Controllers/CartController.cs
+++ Fishing/Areas/Customer/Controllers/CartController.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Fishing.Models;
 using Stripe;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Fishing.Areas.Customer.Controllers
 {
@@ -210,9 +211,9 @@ namespace Fishing.Areas.Customer.Controllers
             }
 
             await _db.SaveChangesAsync();
-            return RedirectToAction("Index", "Home");
+            // return RedirectToAction("Index", "Home");
 
- //     //return RedirectToAction("Confirm","Order", new {id = detail.cart.OrderHeader.Id });
+            return RedirectToAction("Confirm", "Order", new {id = DetailCart.OrderHeader.Id });
 
         }
 
@@ -281,66 +282,9 @@ namespace Fishing.Areas.Customer.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        
 
     }
 }
-
-
-//    public class CartController : Controller
-//    {
-//        private readonly ApplicationDbContext _db;
-//        [BindProperty]
-//        public OrderDetailsCart detailCart { get; set; }
-
-
-
-//        public CartController(ApplicationDbContext db)
-//        {
-//            _db = db;
-//        }
-
-//        public async Task<IActionResult> Index()
-//        {
-//            detailCart = new OrderDetailsCart()
-//            {
-//                OrderHeader = new Models.OrderHeader()
-//            };
-
-//            detailCart.OrderHeader.OrderTotal = 0;
-
-//            var claimsIdentity = (ClaimsIdentity)User.Identity;
-//            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
-
-//            var cart = _db.ShoppingCart.Where(c => c.ApplicationUserId == claim.Value);
-//            if(cart !=null)
-//            {
-//                detailCart.listCart = cart.ToList();
-//            }
-
-//            foreach(var list in detailCart.listCart)
-//            {
-//                list.MenuItem = await _db.MenuItem.FirstOrDefaultAsync(m => m.Id == list.MenuItemId);
-//                detailCart.OrderHeader.OrderTotal = detailCart.OrderHeader.OrderTotal + (list.MenuItem.Price * list.Count);
-
-//                list.MenuItem.Description = SD.ConvertToRawHtml(list.MenuItem.Description);
-
-//                if(list.MenuItem.Description.Length>100)
-//                {
-//                    list.MenuItem.Description = list.MenuItem.Description.Substring(0, 99) + "...";
-//                }
-//            }
-
-//            detailCart.OrderHeader.OrderTotalOriginal = detailCart.OrderHeader.OrderTotal;
-
-
-
-//        }
-//    }
-//}
-
-
-
-
-
 
 
---
+++ Fishing/Areas/Customer/Controllers/OrderController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Fishing.Data;
+using Fishing.Models;
+using Fishing.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fishing.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class OrderController : Controller
+    {
+        private ApplicationDbContext _db;
+        private int PageSize = 2;
+        public OrderController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Confirm(int id)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            OrderDetailsViewModel orderDetailsViewModel = new OrderDetailsViewModel()
+            {
+                OrderHeader = await _db.OrderHeader.Include(o => o.ApplicationUser).FirstOrDefaultAsync(o => o.Id == id && o.UserId == claim.Value),
+                OrderDetails = await _db.OrderDetails.Where(o => o.OrderId == id).ToListAsync()
+            };
+
+            return View(orderDetailsViewModel);
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> OrderHistory(int productPage = 1)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            OrderListViewModel orderListVM = new OrderListViewModel()
+            {
+                Orders = new List<OrderDetailsViewModel>()
+            };
+            
+            
+   //         List<OrderDetailsViewModel> orderList = new List<OrderDetailsViewModel>();
+
+            List<OrderHeader> OrderHeaderList = await _db.OrderHeader.Include(o => o.ApplicationUser).Where(u => u.UserId == claim.Value).ToListAsync();
+
+            foreach (OrderHeader item in OrderHeaderList)
+            {
+                OrderDetailsViewModel individual = new OrderDetailsViewModel
+                {
+                    OrderHeader = item,
+                    OrderDetails = await _db.OrderDetails.Where(o => o.OrderId == item.Id).ToListAsync()
+                };
+                orderListVM.Orders.Add(individual);
+            }
+
+            var count = orderListVM.Orders.Count;
+            orderListVM.Orders = orderListVM.Orders.OrderByDescending(p => p.OrderHeader.Id)
+                                .Skip((productPage - 1) * PageSize)
+                                .Take(PageSize).ToList();
+
+            orderListVM.PagingInfo = new PagingInfo
+            {
+                CurrentPage=productPage,
+                ItemsPerPage=PageSize,
+                TotalItem=count,
+                UrlParam="/Customer/Order/OrderHistory?productPage=:"
+            };
+
+
+            return View(orderListVM);
+        }
+
+        public async Task<IActionResult> GetOrderDetails(int Id)
+        {
+            OrderDetailsViewModel orderDetailsViewModel = new OrderDetailsViewModel()
+            {
+                OrderHeader = await _db.OrderHeader.Include(el => el.ApplicationUser).FirstOrDefaultAsync(m => m.Id == Id),
+                OrderDetails = await _db.OrderDetails.Where(m => m.OrderId == Id).ToListAsync()
+            };
+            //orderDetailsViewModel.OrderHeader.ApplicationUser = await _db.ApplicationUser.FirstOrDefaultAsync(u => u.Id == orderDetailsViewModel.OrderHeader.UserId);
+
+            return PartialView("_IndividualOrderDetails", orderDetailsViewModel);
+        }
+
+
+    }
+}
